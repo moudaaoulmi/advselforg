9673b0b47952a49b983663094892a8b2c63d0ec7
patrick.sannes <patrick.sannes@ee0f3e78-cd7f-11de-af9b-21f9cdd2bce9>
2009-11-24 23:09:34 +0000

    lalalala

--- trunk/ASO-NEXT/src/org/vu/advselforg/InitPlan.java
+++ trunk/ASO-NEXT/src/org/vu/advselforg/InitPlan.java
@@ -1,8 +1,13 @@
 package org.vu.advselforg;
 
+import sun.awt.EmbeddedFrame;
+import jadex.adapter.fipa.AgentIdentifier;
+import jadex.adapter.fipa.SFipa;
+import jadex.runtime.IFilter;
+import jadex.runtime.IMessageEvent;
 import jadex.runtime.Plan;
 
-public class InitPlan extends Plan{
+public class InitPlan extends Plan implements Runnable{
 	
 	private static final long serialVersionUID = 2961883448970106007L;
 	NxtController robot;
@@ -13,7 +18,7 @@ public class InitPlan extends Plan{
 		int rightMotorPort = 3;
 		boolean motorReverse = false;
 		SensorType port1 = SensorType.TOUCH;
-		SensorType port2 = SensorType.NONE;
+		SensorType port2 = SensorType.TOUCH;
 		SensorType port3 = SensorType.NONE;
 		SensorType port4 = SensorType.NONE;
 		try {
@@ -23,12 +28,34 @@ public class InitPlan extends Plan{
 			System.out.println("doet ut niet");
 		}
 
+		new Thread(this).start();
+		
 		getBeliefbase().getBelief("robot").setFact(robot);
 		System.out.println("Connected");
 		
 		getBeliefbase().getBelief("notInitialized").setFact(false);
+		waitFor(IFilter.NEVER);
 		
 	}
 	
-
+	
+	@Override
+	public void run() {
+		while(true){
+			if(robot.getTouchSensorPressed(0)){
+			
+				IMessageEvent me = getExternalAccess().createMessageEvent("berichtSend");
+				me.getParameterSet(SFipa.RECEIVERS).addValue(new AgentIdentifier("Mindstorms",true));
+				me.setContent("TouchSensorStatus " + "Pressed 1");
+				getExternalAccess().sendMessage(me);
+			}
+			if(robot.getTouchSensorPressed(1)){
+				IMessageEvent me = getExternalAccess().createMessageEvent("berichtSend");
+				me.getParameterSet(SFipa.RECEIVERS).addValue(new AgentIdentifier("Mindstorms",true));
+				me.setContent("TouchSensorStatus " + "Pressed 2");
+				getExternalAccess().sendMessage(me);
+			}
+		}
+	}		
 }
+
