2e2697008c73e189e9b99ffdcd5b97f1aa3c0b04
patrick.sannes <patrick.sannes@ee0f3e78-cd7f-11de-af9b-21f9cdd2bce9>
2009-11-24 23:09:34 +0000

    lalalala

--- ASO-NEXT/src/org/vu/advselforg/DrivePlan.java
+++ ASO-NEXT/src/org/vu/advselforg/DrivePlan.java
@@ -1,12 +1,17 @@
 package org.vu.advselforg;
 
 
+import jadex.runtime.IMessageEvent;
 import jadex.runtime.Plan;
 
 public class DrivePlan extends Plan{
 
 	@Override
 	public void body() {
+		
+		IMessageEvent em = (IMessageEvent)getInitialEvent();
+		String data = em.getContent().toString();
+		System.out.println("received "+data);
 		// TODO Auto-generated method stub
 		//RobotController robot = (RobotController)getBeliefbase().getBelief("robot");
 		//robot.moveForward(0, true);
