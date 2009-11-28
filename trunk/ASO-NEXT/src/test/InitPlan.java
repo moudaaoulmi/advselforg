9208a98ad07c2632e9add01f8a1e9eecf3661fb2
patrick.sannes <patrick.sannes@ee0f3e78-cd7f-11de-af9b-21f9cdd2bce9>
2009-11-28 09:09:30 +0000

    aa

---
+++ trunk/ASO-NEXT/src/test/InitPlan.java
@@ -0,0 +1,70 @@
+package test;
+
+import org.vu.advselforg.robotcontroller.*;
+
+import sun.awt.EmbeddedFrame;
+import jadex.adapter.fipa.AgentIdentifier;
+import jadex.adapter.fipa.SFipa;
+import jadex.runtime.IFilter;
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+
+public class InitPlan extends Plan implements Runnable{
+	
+	private static final long serialVersionUID = 2961883448970106007L;
+	NxtController robot;
+	String agentName;
+	
+	public InitPlan(String agentName){
+		this.agentName = agentName;
+
+	}
+	
+	@Override
+	public void body() {
+			
+		int leftMotorPort = 1;
+		int rightMotorPort = 3;
+		boolean motorReverse = false;
+		SensorType port1 = SensorType.TOUCH;
+		SensorType port2 = SensorType.TOUCH;
+		SensorType port3 = SensorType.NONE;
+		SensorType port4 = SensorType.NONE;
+		try {
+			robot = new NxtController("Patrick", leftMotorPort, rightMotorPort,
+					motorReverse, port1, port2, port3, port4);	
+		} catch (Exception e) {
+			System.out.println("doet ut niet");
+		}
+
+		new Thread(this).start();
+		
+		getBeliefbase().getBelief("robot").setFact(robot);
+		System.out.println("Connected");
+		
+		getBeliefbase().getBelief("notInitialized").setFact(false);
+		waitFor(IFilter.NEVER);
+		
+	}
+	
+	
+	@Override
+	public void run() {
+		while(true){
+			if(robot.getTouchSensorPressed(0)){
+			
+				IMessageEvent me = getExternalAccess().createMessageEvent("MessageSend");
+				me.getParameterSet(SFipa.RECEIVERS).addValue(new AgentIdentifier(agentName,true));
+				me.setContent("TouchSensorStatus " + "Pressed 1");
+				getExternalAccess().sendMessage(me);
+			}
+			if(robot.getTouchSensorPressed(1)){
+				IMessageEvent me = getExternalAccess().createMessageEvent("MessageSend");
+				me.getParameterSet(SFipa.RECEIVERS).addValue(new AgentIdentifier(agentName,true));
+				me.setContent("TouchSensorStatus " + "Pressed 2");
+				getExternalAccess().sendMessage(me);
+			}
+		}
+	}		
+}
+
